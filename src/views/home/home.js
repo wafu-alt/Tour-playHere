9579e59419d1c9d7ea03364172153d2832fe856f
[SW/부산] 박태훈 <ekdh0858@naver.com>
2022-06-02 17:29:04 +0900

    로그 아웃 기능 추가 ( 회원정보 수정 기능 js 작성중..집가서 마저 하기 위한 커밋)

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -127,10 +127,21 @@ function onHoverScrollArrow(direction) {
   itemScrollListDiv.scrollBy({ left: distance, behavior: "smooth" });
 }
 
-let loginCheck = document.querySelector("#loginCheck");
+let loginCheck = document.querySelector("#navbar");
 
 if (sessionStorage.getItem("token")) {
-  loginCheck.innerHTML = `<a href="/account">계정관리</a>`;
+  loginCheck.innerHTML = `
+  <li><a href="/account">계정관리</a></li>
+  <li><a href="/account">로그아웃</a></li>
+  <li>
+      <a href="#cart" aria-current="page">
+        <span class="icon">
+          <i class="fas fa-cart-shopping"></i>
+        </span>
+        <span>카트</span>
+      </a>
+  </li>
+  `;
 }
 
 // async function getDataFromApi() {
