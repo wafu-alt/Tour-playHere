063688ba5d19e3464500c0ba303b60ef49e88ddc
[SW/부산] 박태훈 <ekdh0858@naver.com>
2022-06-02 15:52:47 +0900

    이동한 페이지에 따라 내용이 변하는 네비게이션 바 기능 components에 추가

---
+++ src/views/components/account_navbar/account_in.js
@@ -0,0 +1,16 @@
+let loginCheck = document.querySelector("#navbar");
+
+if (sessionStorage.getItem("token")) {
+  loginCheck.innerHTML = `
+    <li><a href="/account">계정관리 </a></li>
+    <li><a href="/logOut"> 로그아웃 </a></li>
+    <li>
+              <a href="/cart" aria-current="page">
+                <span class="icon">
+                  <i class="fas fa-cart-shopping"></i>
+                </span>
+                <span>카트</span>
+              </a>
+    </li>
+    `;
+}
