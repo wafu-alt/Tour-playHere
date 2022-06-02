deef32154e6b8f713491ce219a4bc977e6410689
[SW2/부산] 박태훈 <ekeh0858@naver.com>
2022-06-02 11:04:27 +0900

    view-router 충돌 해결

--- src/routers/views-router.js
+++ src/routers/views-router.js
@@ -6,21 +6,12 @@ const viewsRouter = express.Router();
 // 페이지별로 html, css, js 파일들을 라우팅함
 // 아래와 같이 하면, http://localhost:5000/ 에서는 views/home/home.html 파일을,
 // http://localhost:5000/register 에서는 views/register/register.html 파일을 화면에 띄움
-<<<<<<< HEAD
-viewsRouter.use("/", serveStatic("home"));
-viewsRouter.use("/register", serveStatic("register"));
-viewsRouter.use("/login", serveStatic("login"));
-viewsRouter.use("/list", serveStatic("list"));
-=======
 viewsRouter.use('/', serveStatic('home'));
 viewsRouter.use('/register', serveStatic('register'));
 viewsRouter.use('/login', serveStatic('login'));
+viewsRouter.use("/list", serveStatic("list"));
 viewsRouter.use('/account', serveStatic('account'));
-viewsRouter.use('/account/orders', serveStatic('order'));
-viewsRouter.use('/account/userupdate', serveStatic('userInfoupdate'));
-viewsRouter.use('/product/sell', serveStatic('Sell'));
-viewsRouter.use('/account/deleteAccount', serveStatic('deleteAccount'));
->>>>>>> feature-frontend-account
+
 
 // views 폴더의 최상단 파일인 rabbit.png, api.js 등을 쓸 수 있게 함
 viewsRouter.use("/", serveStatic(""));
