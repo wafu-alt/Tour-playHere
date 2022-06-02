a1812dcc280e019ece9d5fcfb2013aefa59a1703
donghwaKIM <johnjskim92@gmail.com>
2022-06-02 15:23:25 +0900

    test

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -127,7 +127,7 @@ function onHoverScrollArrow(direction) {
   itemScrollListDiv.scrollBy({ left: distance, behavior: "smooth" });
 }
 
-<<<<<<< HEAD
+// <<<<<<< HEAD
 // async function getDataFromApi() {
 //   // 예시 URI입니다. 현재 주어진 프로젝트 코드에는 없는 URI입니다.
 //   const data = await Api.get("/api/user/data");
@@ -136,9 +136,9 @@ function onHoverScrollArrow(direction) {
 //   console.log({ data });
 //   console.log({ random });
 // }
-=======
+// =======
 
 
 
 
->>>>>>> feature-frontend-account
+// >>>>>>> feature-frontend-account
