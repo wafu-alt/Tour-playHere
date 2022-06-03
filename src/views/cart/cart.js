1ee2fbfbb2c230eefceb4fedd70d6f7f9e3d9bb5
Wafu <dacapolin@gmail.com>
2022-06-03 20:46:41 +0900

    js통신 주석처리함

--- src/views/cart/cart.js
+++ src/views/cart/cart.js
@@ -1,12 +1,12 @@
-import * as Api from "../api.js";
-import mongoose from "mongoose";
-const DB_URL =
-  process.env.MONGODB_URL ||
-  "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n";
-console.log(DB_URL);
+// import * as Api from "../api.js";
+// import mongoose from "mongoose";
+// const DB_URL =
+//   process.env.MONGODB_URL ||
+//   "MongoDB 서버 주소가 설정되지 않았습니다.\n./db/index.ts 파일을 확인해 주세요. \n.env 파일도 필요합니다.\n";
+// console.log(DB_URL);
 
-console.log(Api.get);
-console.log(Api.get("api", packagelist));
+// console.log(Api.get);
+// console.log(Api.get("api", packagelist));
 
 // async function call() {
 //   const res = await fetch("http://localhost:5000/api/packagelist");
