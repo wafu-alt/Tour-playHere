fc8eac518ca7fdc7ad6b2cd84de90e0a4ac47a1e
TaeHun <ekdh0858@naver.com>
2022-06-08 00:50:13 +0900

    로그인 네비게이션 바 한개 파일로 수정, 관리자 임명 페이지 추가, 관리자의 조회 페이지 로드 기능 추가, 로그인후 전 페이지로 이동 오류 수정.

--- src/views/account/orders/orders.js
+++ src/views/account/orders/orders.js
@@ -2,45 +2,50 @@ const ordersContainer = document.querySelector("#ordersContainer");
 
 // 로그인한 사용자의 주문을 조회하는 페이지 로딩
 async function loadPage() {
-  //console.log("시작");
+ 
   const fetchData = await fetch("../orders_sample.json").then((response) =>
     response.json()
   );
-  // TODO : 모든 주문서?? 들을 볼수있는 api활용하여 데이터 가져오기
-  //         그 가져온 데이터 에서 로그인한 사람의 데이터를 가져오기
+  // TODO : 모든 주문서?? 들을 볼수있는 api활용해서 데이터 가져오기
+  //  innerHTML안의 data를 다루는 부분도 그에따라 수정해야함
+  //  가져온 데이터 에서 로그인한 사람의 데이터를 가져오기
+           
 
     fetchData.forEach((data) => {
         ordersContainer.innerHTML += `<div class="columns orders-item" id="${data.index}">
         <div class="column is-2">${data.departureDate}</div>
-        <div class="column is-6 order-summary">${data.productName} / 1개</div>
+        <div class="column is-6 order-summary">${data.productName}</div>
         <div class="column is-2">상품 준비중</div>
         <div class="column is-2">
           <button class="button" id="deleteButton-${data.index}">주문 취소</button>
         </div>
       </div>`
-    //console.log(data);
+    
   });
 }
 
 
 
 // 로그인한 사용자가 관리자 일때 모든 주문을 조회하는 페이지 로딩
-const adminPageLoading = () => {
-    //console.log("시작");
-    // TODO : 모든 주문서?? 들을 볼수있는 api활용해서 데이터 가져오기
+async function adminPageLoading() {
+    
+  const fetchData = await fetch("../orders_sample.json").then((response) =>
+    response.json()
+  );
 
     fetchData.forEach((data) => {
         ordersContainer.innerHTML += `<div class="columns orders-item" id="${data.index}">
-        <div class="column is-2">${data.departureDate}</div>
-        <div class="column is-6 order-summary">${data.productName} / 1개</div>
+        <div class="column is-2">관리자 페이지</div>
+        <div class="column is-6 order-summary">의 내용입니다.</div>
         <div class="column is-2">상품 준비중</div>
         <div class="column is-2">
           <button class="button" id="deleteButton-${data.index}">주문 취소</button>
         </div>
       </div>`
-    //console.log(data);
+    
   });
 }
 
-if (sessionStorage.getItem("isAdmin")) { adminPageLoading(); }
+if (sessionStorage.getItem("isLogin") == "ekdh0858@naver.com") { adminPageLoading(); }
+// // 관리자 페이지를 확인하고 싶으시다면 if조건문 안에 이메일을 로그인할 이메일로 바꿔주심 됩니다.
 else { loadPage(); }
