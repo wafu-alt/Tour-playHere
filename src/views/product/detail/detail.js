09bb62a1d8ae9649fb0bbc7b6e620dd11556d4ea
Wafu <dacapolin@gmail.com>
2022-06-07 12:58:30 +0900

    detail 수정중

--- src/views/product/detail/detail.js
+++ src/views/product/detail/detail.js
@@ -1,63 +1,23 @@
-// import { renderItemDetail } from "./detail-com.js";
-//console.log(renderItemDetail());
+import * as Api from "/api.js";
 
-// let data = fetch("./detail-sampleData.json")
-// .then (res => res.json())
-// .then (value => {
-//     console.log(value[0])
-//     data = value[0]
-//     return data
-// })
-// .catch((err) => console.log(err))
 
 async function Data() {
-  let res = await fetch("./detail-sampleData.json");
+ 
+  console.log(window.location.pathname) 
+  // result : /product/detail/629dbba1251a88738ef214b6/
+  
+  const urlPathname = window.location.pathname;
+  const productId = urlPathname.split("/").filter((element,i) => element !== "")[2]
+  console.log(productId) //62996552d8674984f2b07073
 
-  let resData = await res.json();
-  const data = resData.filter(
-    (data) => data.pacakge_id === "6278ad6f927a0d0520ff626a"
-  );
+  /* 상품 정보 불러오기 */
+  const res = await Api.get("/api/package", productId);
+  const {packageName  ,days, totalNumber, imgUrl, substance} = res
+  const price = res.price.toLocaleString('ko-KR')
+  const departureAt = res.departure.split("T")[0]
 
-  console.log(data);
-  const addToken = {
-    // _id: `${data.pacakge_id}`,
-    // departureAt: `${startDaysInput}`,
-    // days: `${data.days}`,
-    // arrivalAt: `${startDaysInput + data.days}`,
-    id : 123124,
-    dsfa : "dafea",
-  };
-
-  sessionStorage.setItem("cartToken", JSON.stringify(addToken));
-  console.log(sessionStorage.getItem("cartToken", addToken));
-
-  //return data[0];
-  //console.log(data[0].images)
-
+  /* 아래 html을 삽입 */
   const body = document.querySelector("body");
-
-  // 여행 출발일 input값 현재 날짜로 고정
-  function date() {
-    let curDate = new Date();
-    let year = curDate.getFullYear();
-    let month = "";
-    let date = "";
-    if (curDate.getMonth() + 1 < 10) {
-      month = `0${curDate.getMonth() + 1}`;
-    } else {
-      month = curDate.getMonth();
-    }
-
-    if (curDate.getDate() < 10) {
-      date = `0${curDate.getDate()}`;
-    } else {
-      date = curDate.getDate();
-    }
-
-    return `${year}-${month}-${date}`;
-  }
-  const curDate = date();
-
   body.insertAdjacentHTML(
     "beforeend",
 
@@ -65,23 +25,23 @@ async function Data() {
       <section id="inner" class="columns" >
         <div id="imgArea" class="column is-half">
           <figure class="image is-400x400">
-            <img src="${data[0].images}" alt="제주도 상품">
+            <img src="${imgUrl}" alt="상품 이미지">
           </figure>
         </div>
         <div id="textArea" class="column is-half">
           <div class="mb-6">
-            <h3 class="title is-3">${data[0].pacakge_name}</h3>
+            <h3 class="title is-3">${days-1}박${days}일의 ${packageName}여행</h3>
             <hr>
             <div id="tourDes">
-              <p class="title is-5 has-text-left">${data[0].price}원</p>
-              <p>여행 간단한 설명</p>
+              <p class="title is-5 has-text-left">${price}원</p>
+              <p>${substance}</p>
             </div>
             <hr class="mt-6">
             <div class="mt-1 pt-1">
               <label id="howPerson" for="howPerson">인 원</label>
-              <input id="howPersonInput" class="input is-info" type="text" placeholder="몇명이신가요?" ><br>
+              <input id="howPersonInput" class="input is-info" type="text" placeholder="최대 ${totalNumber}명입니다." ><br>
               <label id="startDays" for="start">출발일 선택</label>
-              <input id="startDaysInput" type="date" name="start"  value="${curDate}" >
+              <input id="startDaysInput" type="date" name="start"  value="${departureAt}" >
             </div>
           </div>
         </div>
@@ -100,6 +60,7 @@ async function Data() {
   `
   );
 
+  /* 버튼을 위한 dom 가져오기 */
   const cartAddBtn = document.querySelector("#cartAddBtn");
   const orderBtn = document.querySelector("#orderBtn");
   const howPersonInput = document.querySelector("#howPersonInput");
@@ -112,8 +73,7 @@ async function Data() {
       return true;
     }
     alert(`로그인된 사용자만 사용 가능합니다.`);
-    window.location.href = `/login`; //-> veiws/login/login.html
-    return false;
+        return false;
   }
 
   //인원 체크하는 기능
@@ -127,6 +87,11 @@ async function Data() {
       return false;
     }
     return true;
+  }
+
+  //출발일 이전 선택 체크하는 기능
+  function beforedDepartureAt () {
+    
   }
 
   //장바구니추가 버튼 기능
@@ -161,3 +126,19 @@ async function Data() {
   orderBtn.addEventListener("click", orderFnc);
 }
 Data();
+
+
+/*
+//버튼 클릭시 토큰 생성
+const addToken = {
+  // _id: `${data.pacakge_id}`,
+  // departureAt: `${startDaysInput}`,
+  // days: `${data.days}`,
+  // arrivalAt: `${startDaysInput + data.days}`,
+  id : 123124,
+  dsfa : "dafea",
+};
+
+// sessionStorage.setItem("cartToken", JSON.stringify(addToken));
+// console.log(sessionStorage.getItem("cartToken", addToken));
+*/
